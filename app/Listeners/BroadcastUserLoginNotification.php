ab3552500d40fdf0fd69947fed21ab44b23123fe
Abbass <abbassmortazavi@gmail.com>
2022-06-13 19:20:52 +0430

    config AppServiceProvider

--- app/Listeners/BroadcastUserLoginNotification.php
+++ app/Listeners/BroadcastUserLoginNotification.php
@@ -2,10 +2,12 @@
 
 namespace App\Listeners;
 
+use App\Events\UserSessionChanged;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class BroadcatUserLoginNotification
+class BroadcastUserLoginNotification
 {
     /**
      * Create the event listener.
@@ -20,11 +22,11 @@ class BroadcatUserLoginNotification
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param Login $event
      * @return void
      */
-    public function handle($event)
+    public function handle(Login $event)
     {
-        //
+        broadcast(new UserSessionChanged("{$event->user->name} is online" , 'success'));
     }
 }
